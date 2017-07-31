fa5c72c19e35cd14715fca4e24b0a01cc654d293
Jeremy List <quick.dudley@gmail.com>
2017-07-31 21:35:45 +1200

    Web server runs, but web app still undefined.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Network.Wai.Handler.Warp
+import Network.Wai.Handler.WarpTLS
+
+import JobLeadsDiary.Database
+import JobLeadsDiary.Frontend.Types
+import JobLeadsDiary.Frontend.React
+
+myTLSSettings = tlsSettings "selfsigned.crt" "selfsigned.key"
+mySettings = setPort 8043 defaultSettings
+
 main :: IO ()
-main = undefined
+main = runTLS myTLSSettings mySettings undefined
